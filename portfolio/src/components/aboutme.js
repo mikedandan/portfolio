808f7ebaf49530344a43bc1709ea44c39bea5538
unknown <mikedandan2@gmail.com>
2020-02-10 16:04:25 -0800

    added about message

--- portfolio/src/components/aboutme.js
+++ portfolio/src/components/aboutme.js
@@ -4,8 +4,8 @@ import React, { Component } from 'react';
 class About extends Component {
   render() {
     return(
-      <div><h1>About Page</h1></div>
-      <div><h2>testing this update</h2></div>
+      <div><h1>About Page</h1>
+      <h2>hello</h2></div>
     )
   }
 }
